ab3bb80185bdd54fadfd5953b804dc9e23a6687b
Timur Duehr <tduehr@gmail.com>
2018-07-06 13:21:07 -0500

    generalize audit

---
+++ support/cas-server-support-audit-core/src/main/java/org/apereo/cas/audit/AbstractAuditTrailManager.java
@@ -0,0 +1,41 @@
+package org.apereo.cas.audit;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.apereo.inspektr.audit.AuditActionContext;
+import org.apereo.inspektr.audit.AuditTrailManager;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * This is {@link AbstractAuditTrailManager}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.2.0
+ */
+@Slf4j
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class AbstractAuditTrailManager implements AuditTrailManager {
+
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    protected boolean asynchronous;
+
+    @Override
+    public void record(final AuditActionContext audit) {
+        if (this.asynchronous) {
+            this.executorService.execute(() -> saveAuditRecord(audit));
+        } else {
+            saveAuditRecord(audit);
+        }
+    }
+
+    protected abstract void saveAuditRecord(final AuditActionContext audit);
+}
